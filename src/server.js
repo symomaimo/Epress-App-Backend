99f8bd5737123b79a91072b5059c35c18939bb37
symomaimo <kanyisimon@gmail.com>
2025-04-28 13:22:30 +0300

    changes

--- src/server.js
+++ src/server.js
@@ -4,14 +4,13 @@ const port = process.env.PORT || 5000;
 require("dotenv").config();
 const mongoose = require("mongoose");
 const connectDB = require("./database/db");
-const cors = require('cors'); // <-- add this
+const cors = require("cors"); // <-- add this
 
 //import routes
 const parentRoutes = require("./routes/parent/parentRoutes");
 const studentRoutes = require("./routes/student/StudentRoutes");
 const classRoutes = require("./routes/class/ClassRoutes");
 const feesRoutes = require("./routes/fee/FeesRoutes");
-
 
 //COONNECT TO MONGODB
 connectDB();
@@ -27,12 +26,13 @@ app.get("/", (req, res) => {
 
 //use routes
 
-
 app.use("/parents", parentRoutes);
 app.use("/students", studentRoutes);
 app.use("/classes", classRoutes);
-app.use("/fees", feesRoutes );
+app.use("/fees", feesRoutes);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+
+module.exports = app;
